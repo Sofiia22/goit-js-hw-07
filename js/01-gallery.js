8a6c4c8f8884f849144085a4d28d7727d586e03c
Sofiia22 <108904334+Sofiia22@users.noreply.github.com>
2023-01-06 22:14:34 +0200

    update

---
+++ js/01-gallery.js
@@ -0,0 +1,44 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const galleryElem = document.querySelector(".gallery");
+
+const markup = galleryItems
+.map(
+    (item) => `<div class="gallery__item">
+<a class="gallery__link" href="${item.original}">
+  <img
+    class="gallery__image"
+    src="${item.preview}"
+    data-source="${item.original}"
+    alt="${item.description}"
+  />
+</a>
+</div>`
+  )
+    .join("");
+  galleryElem.insertAdjacentHTML("beforeend", markup);
+
+galleryElem.addEventListener("click", onImageClick);
+
+function onImageClick(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`
+  <img src = "${e.target.dataset.source}" width="800" height="600">
+`);
+  instance.show();
+
+  galleryElem.addEventListener("keydown", onKeyClose);
+
+  function onKeyClose(e) {
+    if (e.code === "Escape") {
+      instance.close();
+      galleryElem.removeEventListener("keydown", onKeyClose);
+    }
+  }
+}
+
+console.log(galleryItems);
